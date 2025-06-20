aef0e57c16e65cb04a1565d3509118afc291cd70
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-06-21 01:56:26 +0800

    cleandata质量评估，使用fastqc和multiqc
    kneaddata已经对质控后的cleandata执行质量评估，
    从质控结果中提取出cleandata的评估报告，
    使用multiqc汇总评估报告。

---
+++ 01_raw_sequence_preprocessing/04_quality_assessment_cleandata.sh
@@ -0,0 +1,27 @@
+mkdir -p 04_quality_assessment_cleandata/fastqc_paired
+mkdir -p 04_quality_assessment_cleandata/fastqc_single
+
+# 动态获取Conda路径
+CONDA_BASE=$(conda info --base)
+# 加载Conda基础配置
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+
+# 获取评估报告
+sample=$(ls 02_quality_control_rawdata)
+
+for i in $sample
+do
+    mv 02_quality_control_rawdata/${i}/fastqc/${i}_L1_1_kneaddata_paired* 04_quality_assessment_cleandata/fastqc_paired/ &
+    mv 02_quality_control_rawdata/${i}/fastqc/${i}_L1_1_kneaddata_unmatched* 04_quality_assessment_cleandata/fastqc_single/ &
+done
+wait
+
+# 汇总评估报告
+conda activate multiqc
+
+multiqc -d 04_quality_assessment_cleandata/fastqc_paired/ -o 04_quality_assessment_cleandata/fastqc_paired/
+multiqc -d 04_quality_assessment_cleandata/fastqc_single/ -o 04_quality_assessment_cleandata/fastqc_single/
+
+# 求(mean ± SD），file的第五列是每个样本的 Total Sequences
+file="./04_quality_assessment_cleandata/fastqc_paired/multiqc_data/multiqc_fastqc.txt"
+awk -F'\t' 'NR>1 {sum+=$5; sumsq+=$5*$5; count++} END {mean=sum/count; sd=sqrt(sumsq/count - (mean*mean)); printf "均值 ± 标准差: %.2f ± %.2f\n", mean, sd}' "$file"
