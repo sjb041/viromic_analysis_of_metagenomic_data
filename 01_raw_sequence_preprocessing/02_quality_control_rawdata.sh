febf1bc1bd2594b1c73c062df303d94da574129f
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-06-21 18:53:31 +0800

    Update 02_quality_control_rawdata.sh
    ** 修改了样本名的获取方式，直接输入样本名
    ** 修改了H样本去宿主使用的参考基因组—— GRCh38.p14
    ** 修改了bowtie2的参数 --bowtie2-options '--end-to-end --sensitive --reorder'

--- 01_raw_sequence_preprocessing/02_quality_control_rawdata.sh
+++ 01_raw_sequence_preprocessing/02_quality_control_rawdata.sh
@@ -1,4 +1,4 @@
-mkdir 02_quality_control_rawdata
+mkdir -p 02_quality_control_rawdata
 
 # 动态获取Conda路径
 CONDA_BASE=$(conda info --base)
@@ -6,19 +6,19 @@ CONDA_BASE=$(conda info --base)
 source "$CONDA_BASE/etc/profile.d/conda.sh"
 conda activate kd0.7.4
 
-# 获取样本名
-sample=$(ls 00_rawdata)
+# 样本名
+sample="F1.1A F1.2A F1.3A F2.1A F2.2A F2.3A FG.1A FG.2A FG.3A L1.1A L1.2A L1.3A L2.1A L2.2A L2.3A H.LX H.O"
 
 for i in $sample
 do
 	in1="00_rawdata/${i}/${i}_L1_1.fq.gz"
 	in2="00_rawdata/${i}/${i}_L1_2.fq.gz"
 	outdir="02_quality_control_rawdata/${i}"
-	mkdir ${outdir}
+	mkdir -p ${outdir}
 
-    # 判断使用哪个宿主参考库
+    # H样本去除宿主 hg_38_p14，其他样本去除宿主 mouse_C57BL_6NJ
     if [ "$i" = "H.LX" ] || [ "$i" = "H.O" ]; then
-        db_path="$HOME/db/kneaddata_db/hg_39"
+        db_path="$HOME/db/kneaddata_db/hg_38_p14"
     else
         db_path="$HOME/db/kneaddata_db/mouse_C57BL_6NJ"
     fi
@@ -27,10 +27,10 @@ do
 		-db ${db_path} \
 		--trimmomatic $HOME/miniconda3/envs/kd0.7.4/share/trimmomatic \
 		--trimmomatic-options 'ILLUMINACLIP:$HOME/miniconda3/envs/kd0.7.4/share/trimmomatic/adapters/TruSeq3-PE.fa:2:40:15 SLIDINGWINDOW:4:20 MINLEN:50' \
-		--bowtie2-options '--very-sensitive --dovetail --reorder' \
+		--bowtie2-options '--end-to-end --sensitive --reorder' \
 		--remove-intermediate-output \
 		--run-fastqc-end \
-		-t 30  
+		-t 30
 
 	kneaddata_read_count_table --input ${outdir} --output ${outdir}/sum.tsv
 done
