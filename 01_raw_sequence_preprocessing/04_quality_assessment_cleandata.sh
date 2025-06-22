89375b41d7d7e0fd473100e47adf5912bcecf10b
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-06-22 20:51:49 +0800

    Update 04_quality_assessment_cleandata.sh
    ** fastqc评估，multiqc合并报告

--- 01_raw_sequence_preprocessing/04_quality_assessment_cleandata.sh
+++ 01_raw_sequence_preprocessing/04_quality_assessment_cleandata.sh
@@ -6,22 +6,31 @@ CONDA_BASE=$(conda info --base)
 # 加载Conda基础配置
 source "$CONDA_BASE/etc/profile.d/conda.sh"
 
-# 样本名
-sample="F1.1A F1.2A F1.3A F2.1A F2.2A F2.3A FG.1A FG.2A FG.3A L1.1A L1.2A L1.3A L2.1A L2.2A L2.3A H.LX H.O"
+# 质量评估
+(
+    conda activate fastqc
 
-# 获取评估报告
-for i in $sample
-do
-    mv 02_quality_control_rawdata/${i}/fastqc/${i}_L1_1_kneaddata_paired* 04_quality_assessment_cleandata/fastqc_paired/ &
-    mv 02_quality_control_rawdata/${i}/fastqc/${i}_L1_1_kneaddata_unmatched* 04_quality_assessment_cleandata/fastqc_single/ &
-done
-wait
+    # 查找要评估的文件，并将文件名存储在变量中
+    files=$(find ./03_cleandata -type f \( -name '*R1.fastq.gz' -o -name '*R2.fastq.gz' \))
+    files_s=$(find ./03_cleandata -type f \( -name '*s1.fastq.gz' -o -name '*s2.fastq.gz' \))
+
+    # 打印看看找到的文件
+    echo "$files"
+    # 使用 GNU Parallel 并行评估
+    echo "$files" | parallel -j 30 'fastqc {} -o 04_quality_assessment_cleandata/fastqc_paired -f fastq -t 2'
+
+    # 打印看看找到的文件
+    echo "$files_s"
+    # 使用 GNU Parallel 并行评估
+    echo "$files_s" | parallel -j 30 'fastqc {} -o 04_quality_assessment_cleandata/fastqc_single -f fastq -t 2'
+)
 
 # 汇总评估报告
-conda activate multiqc
-
-multiqc -d 04_quality_assessment_cleandata/fastqc_paired/ -o 04_quality_assessment_cleandata/fastqc_paired/
-multiqc -d 04_quality_assessment_cleandata/fastqc_single/ -o 04_quality_assessment_cleandata/fastqc_single/
+(
+    conda activate multiqc
+    multiqc -d 04_quality_assessment_cleandata/fastqc_paired -o 04_quality_assessment_cleandata/fastqc_paired/
+    multiqc -d 04_quality_assessment_cleandata/fastqc_single -o 04_quality_assessment_cleandata/fastqc_single/
+)
 
 # 求(mean ± SD），file的第五列是每个样本的 Total Sequences
 file="./04_quality_assessment_cleandata/fastqc_paired/multiqc_data/multiqc_fastqc.txt"
