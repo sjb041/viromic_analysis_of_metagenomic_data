a140042684a1b65f8e6402b61f6ac13e3ec3c4a1
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-06-25 20:27:08 +0800

    Update 01_assembly_get_contigs.sh

--- 02_assembly/01_assembly_get_contigs.sh
+++ 02_assembly/01_assembly_get_contigs.sh
@@ -7,8 +7,9 @@ conda activate spades
 
 mkdir -p 01_assembly_results
 mkdir -p 02_contigs
+mkdir -p 01_assembly_results_addS
+mkdir -p 02_contigs_addS
 
-#sample=("F1_1A" "F2_1A" "FG_1A" "H_LX" "L1_1A" "L2_1A")
 sample="F1_1A F1_2A F1_3A F2_1A F2_2A F2_3A FG_1A FG_2A FG_3A L1_1A L1_2A L1_3A L2_1A L2_2A L2_3A H_LX H_O"
 
 # 组装
@@ -19,14 +20,20 @@ do
     in2="../01_raw_sequence_preprocessing/03_cleandata/${i}_R2.fastq.gz"
     ins="../01_raw_sequence_preprocessing/03_cleandata/${i}_S.fastq.gz"
     out="01_assembly_results/${i}"
+    out_addS="01_assembly_results_addS/${i}"
 
     spades.py --meta -1 $in1  -2 $in2 -o $out -k 21,33,55,77 -m 100 -t 20
-    #spades.py --meta -1 $in1  -2 $in2 -s $ins -k 21,33,55,77 -m 100 -t 20 -o $out 
+    spades.py --meta -1 $in1  -2 $in2 -s $ins -k 21,33,55,77 -m 100 -t 20 -o $out_addS 
 
     # 提取 contigs
     mv ${out}/contigs.fasta 02_contigs/${i}_contigs.fasta
     mv ${out}/scaffolds.fasta 02_contigs/${i}_scaffolds.fasta
+    mv ${out_addS}/contigs.fasta 02_contigs_addS/${i}_contigs.fasta
+    mv ${out_addS}/scaffolds.fasta 02_contigs_addS/${i}_scaffolds.fasta
 
     #rm -r $out
+    #rm -r $out_addS
+    
     #mv $out /mnt/e/2025_2ME/viromic_analysis_of_metagenomic_data/02_assembly/01_assembly_results/
+    #mv $out_addS /mnt/e/2025_2ME/viromic_analysis_of_metagenomic_data/02_assembly/01_assembly_results_addS/
 done
