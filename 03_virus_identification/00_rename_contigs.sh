ffd7cc37a6a170d20f3c191ea1914f281fd3f2ce
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-07-15 01:54:32 +0800

    contig序列重命名，F1_1A_contigs.fasta中序列名改为F1_1A_contigs_1，F1_1A_contigs_2...

---
+++ 03_virus_identification/00_rename_contigs.sh
@@ -0,0 +1,16 @@
+mkdir -p 00_rename_contigs && cd 00_rename_contigs
+
+# 将组装后的 contig 复制过来
+cp ../../02_assembly/02_contigs/*_contigs* ./
+
+# 序列重命名
+for sample in F1_1A F1_2A F1_3A F2_1A F2_2A F2_3A FG_1A FG_2A FG_3A L1_1A L1_2A L1_3A L2_1A L2_2A L2_3A H_LX H_O
+do
+ 
+    # 使用 seqkit 重命名序列
+    seqkit replace -p ".*" -r "${sample}_contig_{nr}" "${sample}_contigs.fasta" -o "${sample}_contigs.fna"
+    echo "${sample} done"
+
+done
+
+rm *.fasta
