3716e4488cba3fe99d7e68f3c59965f31a9d3613
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-07-16 00:40:41 +0800

    Update 01_run_vs2.sh

--- 03_virus_identification/01_run_vs2.sh
+++ 03_virus_identification/01_run_vs2.sh
@@ -5,10 +5,12 @@ source "$CONDA_BASE/etc/profile.d/conda.sh"
 
 conda activate vs2
 
-mkdir -p 02_run_vs2 && cd 02_run_vs2
+mkdir -p 01_run_vs2 && cd 01_run_vs2
 
 time virsorter run -i ../00_rename_contigs/all_contigs.fna -w vs2_keep-l4k-s0.5 --keep-original-seq --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
 
 # real    987m31.932s
 # user    17218m29.797s
 # sys     502m17.363s
+
+time virsorter run -i ../00_rename_contigs/all_contigs.fna -w vs2_l4k-s0.5 --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
