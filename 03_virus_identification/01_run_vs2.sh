21a929289e524a2335b0c872718474b3f4c41c96
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-07-16 19:12:57 +0800

    Update 01_run_vs2.sh

--- 03_virus_identification/01_run_vs2.sh
+++ 03_virus_identification/01_run_vs2.sh
@@ -10,13 +10,13 @@ cat ../02_assembly/02_contigs/rename_contigs/*.fna > all_contigs.fna
 
 mkdir -p 01_run_vs2 && cd 01_run_vs2
 
-time virsorter run -i all_contigs.fna -w vs2_keep-l4k-s0.5 --keep-original-seq --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
+time virsorter run -i ../all_contigs.fna -w vs2_keep-l4k-s0.5 --keep-original-seq --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
 
 # real    987m31.932s
 # user    17218m29.797s
 # sys     502m17.363s
 
-time virsorter run -i all_contigs.fna -w vs2_l4k-s0.5 --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
+time virsorter run -i ../all_contigs.fna -w vs2_l4k-s0.5 --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
 
 # real    957m36.394s
 # user    16872m10.353s
