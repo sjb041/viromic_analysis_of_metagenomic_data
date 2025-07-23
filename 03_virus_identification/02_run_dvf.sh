5052ec1ba0ccc4ddd108e0539eb44cd00514c8f0
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-07-23 21:27:11 +0800

    Update 02_run_dvf.sh

--- 03_virus_identification/02_run_dvf.sh
+++ 03_virus_identification/02_run_dvf.sh
@@ -5,3 +5,11 @@ source "$CONDA_BASE/etc/profile.d/conda.sh"
 
 conda activate DVF
 dvf.py -i all_contigs.fna -o 02_run_dvf -l 4000 -c 13
+
+# 根据分数阈值和长度阈值筛选
+cd 02_run_dvf
+awk 'NR==1 || ($2 >= 4000 &&  $3 > 0.9 && $4 < 0.05)' all_contigs.fna_gt4000bp_dvfpred.txt > gt4000bp_s0.9_p0.05.txt
+# 提取序列
+tail -n +2 gt4000bp_s0.9_p0.05.txt | cut -f 1 | seqtk subseq ../all_contigs.fna - > dvf.fna && grep -c '^>' dvf.fna
+
+cd ..
