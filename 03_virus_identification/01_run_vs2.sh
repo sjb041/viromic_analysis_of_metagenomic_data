8cf2d1c7ce5bb001a42f886b26b1724a7ab7d841
sjb041 <139522488+sjb041@users.noreply.github.com>
2025-07-23 21:12:54 +0800

    Update 01_run_vs2.sh

--- 03_virus_identification/01_run_vs2.sh
+++ 03_virus_identification/01_run_vs2.sh
@@ -3,13 +3,12 @@ CONDA_BASE=$(conda info --base)
 # 加载Conda基础配置
 source "$CONDA_BASE/etc/profile.d/conda.sh"
 
-conda activate vs2
-
 # 获取所有样本的contigs
 cat ../02_assembly/02_contigs/rename_contigs/*.fna > all_contigs.fna
 
 mkdir -p 01_run_vs2 && cd 01_run_vs2
 
+conda activate vs2
 time virsorter run -i ../all_contigs.fna -w vs2_l4k-s0.5 --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
 
 # real    957m36.394s
@@ -17,9 +16,10 @@ time virsorter run -i ../all_contigs.fna -w vs2_l4k-s0.5 --include-groups dsDNAp
 # sys     498m56.884s
 
 # 如果只是想使用--keep-original-seq，其它参数不变，可以根据得分表从all_contigs.fna中获取那些||full序列，而不用重新跑一次
-#time virsorter run -i ../all_contigs.fna -w vs2_keep-l4k-s0.5 --keep-original-seq --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
+time virsorter run -i ../all_contigs.fna -w vs2_keep-l4k-s0.5 --keep-original-seq --include-groups dsDNAphage,ssDNA --min-length 4000 --min-score 0.5 -j 30 all
 
 # real    987m31.932s
 # user    17218m29.797s
 # sys     502m17.363s
 
+cd ..
