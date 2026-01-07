b673a44a6d3afd7dba1a921a397d86f0df2954bb
shijiabin <139522488+sjb041@users.noreply.github.com>
2026-01-07 18:37:09 +0800

    Modify output to include additional tables
    Updated output format to include two tables: one for relative abundance and another for calculation details.

--- 06_relative_abundance/method1.py
+++ 06_relative_abundance/method1.py
@@ -95,7 +95,9 @@ def parse_idxstats(idxstats_file, sample_name, abun_file):
     """
     解析 samtools idxstats 输出文件
     计算相对丰度
-    输出一个表格, 两列, vOTU rel_abun_{sample_name}
+    输出两个表格: 
+        相对丰度表, 2 列, vOTU    rel_abun_{sample_name}
+        计算过程表, 6 列, vOTU    length    mapped    unmapped    count_per_bp    rel_abun_{sample_name}
     """
     data = pd.read_csv(idxstats_file, sep='\t', header=None, 
                        names=['vOTU', 'length', 'mapped', 'unmapped'])
